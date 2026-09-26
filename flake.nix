04b569967c7fb8d973754c340d90501f65612b26
Olavi Äikäs <olavi.aikas@protonmail.com>
2026-09-26 16:24:04 +0300

    Update for fuksiaiset 2026

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+	buildInputs = [
+	  pkgs.entr
+	  pkgs.texlab
+	  pkgs.texliveFull
+	  (pkgs.hunspellWithDicts (with pkgs.hunspellDicts; [ en_GB-ise ]))
+	];
+
+	shellHook = "exec nu";
+      };
+    };
+}
---
+++ watch_build.nu
@@ -0,0 +1,1 @@
+ls *.tex songs/*.tex chapters/*.tex | get name | to text | entr lualatex --halt-on-error YS_laulukirja.tex
